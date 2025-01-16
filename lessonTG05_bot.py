c27ae1e74a285d2e747e7c6b148abdd5393b5930
NATALIA <nata.rozanova80@gmail.com>
2025-01-16 21:09:47 +0300

    Update lessonTG05_bot.py

--- lessonTG05_bot.py
+++ lessonTG05_bot.py
@@ -11,39 +11,6 @@ CRYPTO_API_URL = 'https://api.coingecko.com/api/v3/simple/price'
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-
-#dp = Dispatcher(bot)
-
-
-# @dp.message(Command('crypto'))
-# async def cmd_crypto(message: types.Message):
-#     currencies = message.text.split()[1:]  # Получаем все валюты после команды
-#     if currencies:
-#         prices = []
-#         for currency in currencies:
-#             price_info = get_crypto_price(currency.lower())
-#             prices.append(price_info)
-#         await message.answer("\n".join(prices))  # Отправляем все котировки в одном сообщении
-#     else:
-#         await message.answer(
-#             "Пожалуйста, укажите валюту:\n"
-#             "1. Bitcoin - `bitcoin`\n"
-#             "2. Ethereum - `ethereum`\n"
-#             "3. Ripple - `ripple`\n"
-#             "4. Litecoin - `litecoin`\n"
-#             "5. Bitcoin Cash - `bitcoin-cash`\n"
-#             "6. Cardano - `cardano`\n"
-#             "7. Polkadot - `polkadot`\n"
-#             "8. Dogecoin - `dogecoin`\n"
-#             "9. Chainlink - `chainlink`\n"
-#             "10. Binance Coin - `binancecoin`\n"
-#             "11. Solana - `solana`\n"
-#             "12. Tron - `tron`\n"
-#             "13. Stellar - `stellar`\n"
-#             "14. Tether - `tether`\n"
-#             "15. USD Coin - `usd-coin`."
-#         )
-
 @dp.message(Command('crypto'))
 async def cmd_crypto(message: types.Message):
     price_info = get_crypto_price('bitcoin')  # Получаем котировку биткоина
